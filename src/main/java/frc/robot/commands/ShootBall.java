0b6fb7455d1bdf81f3015687861b08449bf42e57
Bryson24 <bisonmannnn@gmail.com>
2025-02-11 16:41:44 -0600

    started command for shooter

--- src/main/java/frc/robot/commands/ShootBall.java
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -1,5 +1,37 @@
 package frc.robot.commands;
 
-public class ShootBall {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class ShootBall extends Command {
     
+    private Shooter shooter;
+    
+    public ShootBall(Shooter shoot) {
+        shooter = shoot;
+        addRequirements(shooter);
+    }
+    
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {}
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        shooter.setPower(0);
+    }
+    
+    /*
+    @Override
+    public boolean isFinished() {
+        //no ball in shooter????
+    }
+    */
+
 }
